7f96969ec27681876bc000f0ab7185060d7b0de8
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2026-01-03 19:17:10 +0530

    2389. Longest Subsequence With Limited Sum

---
+++ answerQueries.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int[] res = new int[queries.length];
+        for(int i = 1;i<nums.length;i++){
+            nums[i] += nums[i - 1];
+        }
+        for(int i = 0;i<queries.length;i++){
+            int j = Arrays.binarySearch(nums,queries[i]);
+            res[i] = Math.abs(j + 1);
+        }
+        return res;
+    }
+}
